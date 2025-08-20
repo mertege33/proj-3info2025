dde10b293dac69e2e6d186499e3527f95f547e59
gabriel-erkmann <erkmann.g.gabriel@gmail.com>
2025-08-20 16:31:01 -0300

    banco

--- config/db.sql
+++ config/db.sql
@@ -1,62 +1,52 @@
 create schema BioLineage;
 
 CREATE TABLE IF NOT EXISTS usuario (
-  `idusuario` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NULL,
-  `telefone` VARCHAR(45) NULL,
-  `senha` VARCHAR(45) NULL,
-  `dataNascimento` VARCHAR(45) NULL,
-  `Instituicao` VARCHAR(45) NULL,
-  `descricao` VARCHAR(45) NULL,
+  id_usuario INT NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NULL,
+  telefone VARCHAR(45) NULL,
+  senha VARCHAR(45) NULL,
+  dataNascimento VARCHAR(45) NULL,
+  Instituicao VARCHAR(45) NULL,
+  descricao VARCHAR(45) NULL,
   
   
      
-  PRIMARY KEY (`idusuario`))
-ENGINE = InnoDB;
+  PRIMARY KEY (idusuario));
 
 
 
 CREATE TABLE IF NOT EXISTS perfil (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `cor_olho` VARCHAR(45) NOT NULL,
-  `cor_cabelo` VARCHAR(45) NOT NULL,
-  `tipo_sangue` VARCHAR(45) NOT NULL,
-  `tipo_orelha` VARCHAR(45) NULL,
-  `tipo_sanguineo` VARCHAR(45) NULL,
-  `cov_queixo` VARCHAR(45) NULL,
-  `cov_bochecha` VARCHAR(45) NULL,
-  `nacionalidade` VARCHAR(45) NULL,
-  `doenca_genealogica` VARCHAR(45) NULL,
-  `usuariocol` VARCHAR(45) NOT NULL,
-  `usuariocol1` VARCHAR(45) NULL,
-  `usuario_idusuario` INT NOT NULL,
-  `id_pai` INT NOT NULL,
-  `id_mae` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_perfil_usuario_idx` (`usuario_idusuario` ASC) VISIBLE,
-  INDEX `fk_perfil_usuario1_idx` (`id_pai` ASC) VISIBLE,
-  INDEX `fk_perfil_usuario2_idx` (`id_mae` ASC) VISIBLE,
-  CONSTRAINT `fk_perfil_usuario`
-    FOREIGN KEY (`usuario_idusuario`)
-    REFERENCES `mydb`.`usuario` (`idusuario`)
+  id_perfil INT NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  cor_olho VARCHAR(45) NOT NULL,
+  cor_cabelo VARCHAR(45) NOT NULL,
+  tipo_sangue VARCHAR(45) NOT NULL,
+  tipo_orelha VARCHAR(45) NULL,
+  tipo_sanguineo VARCHAR(45) NULL,
+  cov_queixo VARCHAR(45) NULL,
+  cov_bochecha VARCHAR(45) NULL,
+  nacionalidade VARCHAR(45) NULL,
+  doenca_genealogica VARCHAR(45) NULL,
+  usuariocol VARCHAR(45) NOT NULL,
+  usuariocol1 VARCHAR(45) NULL,
+  usuario_idusuario INT NOT NULL,
+  id_pai INT NOT NULL,
+  id_mae INT NOT NULL,
+  PRIMARY KEY (id_perfil),
+
+  CONSTRAINT fk_perfil_usuario
+    FOREIGN KEY (usuario_id_usuario)
+    REFERENCES mydb.usuario (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_perfil_usuario1`
-    FOREIGN KEY (`id_pai`)
-    REFERENCES `mydb`.`usuario` (`idusuario`)
+  CONSTRAINT fk_perfil_usuario1
+    FOREIGN KEY (id_pai)
+    REFERENCES mydb.usuario (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_perfil_usuario2`
-    FOREIGN KEY (`id_mae`)
-    REFERENCES `mydb`.`usuario` (`idusuario`)
+  CONSTRAINT fk_perfil_usuario2
+    FOREIGN KEY (id_mae)
+    REFERENCES mydb.usuario (id_usuario)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-COMMENT = '			';
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    ON UPDATE NO ACTION);
