e23173632cb994bf187228c090e624cd6e5503b2
Manuela <manuelamouta1805@gmail.com>
2025-08-27 16:33:35 -0300

    Atualizações covinhas

--- config/config.inc.php
+++ config/config.inc.php
@@ -7,3 +7,10 @@ define('PORT', '3306'); // porta do mysql
 define('DB', 'xxxxx'); // nome do banco
 define('DSN', "mysql:host=".HOST.";port=".PORT.";dbname=".DB.";charset=UTF8");
 
+try {
+    $pdo = new PDO(DSN, USUARIO, SENHA);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco: " . $e->getMessage());
+}
