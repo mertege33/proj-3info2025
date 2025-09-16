11c243085c7d6c045068a1ecb676fa6d2a1409e8
gabriel-erkmann <erkmann.g.gabriel@gmail.com>
2025-09-16 13:49:06 -0300

    db

--- config/db.sql
+++ config/db.sql
@@ -1,76 +1,59 @@
-create schema BioLineage;
-use BioLineage;
+CREATE SCHEMA IF NOT EXISTS BioLineage;
+USE BioLineage;
+
+-- tabela de doenças precisa vir antes
+CREATE TABLE IF NOT EXISTS doenca (
+  id_doenca INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS usuario (
-  id_usuario INT PRIMARY KEY  auto_increment,
+  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(45) NOT NULL,
-  email VARCHAR(45) NULL,
-  telefone VARCHAR(45) NULL,
-  senha VARCHAR(45) NULL,
-  dataNascimento date NULL,
-  Instituicao VARCHAR(45) NULL,
-  descricao VARCHAR(45) NULL);
+  email VARCHAR(45),
+  telefone VARCHAR(45),
+  senha VARCHAR(45),
+  dataNascimento DATE,
+  instituicao VARCHAR(45),
+  descricao VARCHAR(45)
+);
 
 CREATE TABLE IF NOT EXISTS perfil (
+  id_perfil INT PRIMARY KEY AUTO_INCREMENT,
   sexo ENUM('Feminino','Masculino','Não Binário','Outro') NOT NULL,
-  id_perfil INT  PRIMARY KEY auto_increment,
   cor_olho ENUM('Azul','Castanho','Cinza','Preto','Verde') NOT NULL,
   cor_cabelo ENUM('Branco','Castanho','Loiro','Preto','Ruivo') NOT NULL,
-  tipo_orelha ENUM ('Com divisão','Sem divisão') NULL,
-  tipo_sanguineo ENUM('A','B','AB','O') NULL,
-  daltonismo ENUM('Sim','Não') NULL,
-  sardas ENUM('Sim','Não') NULL,
-  fator ENUM('+','-') NULL,
-  cov_queixo tinyint NULL,
-  cov_bochecha tinyint NULL,
-  albinismo tinyint NULL,
-  nacionalidade VARCHAR(45) NULL,
-  doenca_genealogica int NOT NULL,
+  tipo_orelha ENUM('Com divisão','Sem divisão'),
+  tipo_sanguineo ENUM('A','B','AB','O'),
+  fator ENUM('+','-'),
+  daltonismo ENUM('Sim','Não'),
+  sardas ENUM('Sim','Não'),
+  cov_queixo TINYINT,
+  cov_bochecha TINYINT,
+  albinismo TINYINT,
+  nacionalidade VARCHAR(45),
+  doenca_genealogica INT NOT NULL,
   usuario_idusuario INT NOT NULL,
   id_pai INT NOT NULL,
   id_mae INT NOT NULL,
-  alelo_pai varchar(45),
-  alelo_mae varchar(45),
-    FOREIGN KEY (usuario_idusuario) REFERENCES usuario (id_usuario) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY (id_pai) REFERENCES usuario (id_usuario) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY (id_mae) REFERENCES usuario (id_usuario) ON DELETE NO ACTION ON UPDATE NO ACTION);
-    FOREIGN KEY (doenca_genealogica) REFERENCES doenca (id_doenca) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  alelo_pai VARCHAR(45),
+  alelo_mae VARCHAR(45),
+  FOREIGN KEY (usuario_idusuario) REFERENCES usuario (id_usuario),
+  FOREIGN KEY (id_pai) REFERENCES usuario (id_usuario),
+  FOREIGN KEY (id_mae) REFERENCES usuario (id_usuario),
+  FOREIGN KEY (doenca_genealogica) REFERENCES doenca (id_doenca)
+);
 
--- dados de teste
-    insert into usuario values(null, 'Nome teste','emailteste@mail.com', '12345678','123','2000-06-23','IFC','usuario teste');
-    insert into usuario values(null, 'Pai teste','paiteste@mail.com', '12345678','123','1975-01-15','IFC','usuario teste');
-    insert into usuario values(null, 'Mae teste','maeteste@mail.com', '12345678','123','1980-05-25','IFC','usuario teste');
-    insert into perfil values(null, 'Castanho','Castanho','Sem divisão','A','+',0,0,'Brasileira',0,1,2,3); -- perfil do teste
-    
-	insert into usuario values(null, 'Mãe Mãe teste','maemaeteste@mail.com', '12345678','123','1975-01-15','IFC','usuario teste');
-    insert into usuario values(null, 'Pai Mãe teste','paimaeteste@mail.com', '12345678','123','1975-01-15','IFC','usuario teste');
-    insert into usuario values(null, 'Mãe Pai teste','maepaiteste@mail.com', '12345678','123','1975-01-15','IFC','usuario teste');
-    insert into usuario values(null, 'Pai Pai teste','paipaiteste@mail.com', '12345678','123','1975-01-15','IFC','usuario teste');
-    
+-- exemplo de inserts
+INSERT INTO usuario VALUES
+  (NULL, 'Nome teste','emailteste@mail.com','12345678','123','2000-06-23','IFC','usuario teste'),
+  (NULL, 'Pai teste','paiteste@mail.com','12345678','123','1975-01-15','IFC','usuario teste'),
+  (NULL, 'Mae teste','maeteste@mail.com','12345678','123','1980-05-25','IFC','usuario teste');
 
-    insert into perfil values(null, 'Azul','Loiro','Sem divisão','A','+',0,0,0,'Brasileira',0,2,7,6); -- perfil do pai
-    insert into perfil values(null, 'Castanho','Castanho','Sem divisão','A','+',0,0,0,'Brasileira',0,3,5,4); -- perfil do mae
--- consulta 
- select u.id_usuario,
-        u.nome,
-        pe.id_perfil ,
-        (select m.nome from usuario m where m.id_usuario = pe.id_mae) as Mae,
-        (select p.nome from usuario p where p.id_usuario = pe.id_pai) as Pai
-   from usuario u
-   inner join perfil pe on (pe.usuario_idusuario = u.id_usuario);
+INSERT INTO doenca (nome) VALUES ('Nenhuma'), ('ExemploDoenca');
 
-CREATE TABLE IF NOT EXISTS doenca (
-  id_doenca INT PRIMARY KEY auto_increment,
-  nome VARCHAR(45) NOT NULL);   
-   
-
--- outro exemplo - dessa forma fica mais fácio carregar mais dados com menor qtd de transações
- select u.id_usuario,
-        u.nome,
-        pe.id_perfil ,
-        mae.nome as mae,
-        pai.nome as pai
-   from usuario u
-   inner join perfil pe on (pe.usuario_idusuario = u.id_usuario)
-   left outer join usuario mae on (mae.id_usuario = pe.id_mae)
-   left outer join usuario pai on (pai.id_usuario = pe.id_pai)
+INSERT INTO perfil (sexo, cor_olho, cor_cabelo, tipo_orelha, tipo_sanguineo, fator,
+                    daltonismo, sardas, cov_queixo, cov_bochecha, albinismo,
+                    nacionalidade, doenca_genealogica, usuario_idusuario, id_pai, id_mae)
+VALUES
+('Masculino','Castanho','Castanho','Sem divisão','A','+','Não','Não',0,0,0,'Brasileira',1,1,2,3);
